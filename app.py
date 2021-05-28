71ae0333ac05b62d57f2156581ea4dfe487a125a
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-05-28 14:18:35 +0530

    Html updated

--- app.py
+++ app.py
@@ -11,17 +11,23 @@ def home():
 
 @app.route("/",methods=["POST"])
 def predict():
-    values=[["gender",0],["age",0],["hypertension",0],["heartdisease",0],["residence",0],["glucose",0],["bmi",0],["formellysmoked",0],["neversmoked",0],["smokes",0],["unknown",0]]
+    values=[["gender",0],["age",0],["hypertension",0],["heartdisease",0],["residence",0],["glucose",0],["bmi",0]]
     form_keys=request.form
-    print(form_keys)
+    # print(form_keys)
     features=[]
     for i in values:
         if(i[0] in form_keys):
             features.append(form_keys[i[0]])
+
+            # [features.append(j) for j in smoke_value]
         else:
             features.append(i[1])
  #   features = [int(x) for x in request.form.values()]
 #    print("This is request",features)
+#     elif (i[0] == "smoke"):
+    smoke_value = [0, 0, 0, 0]
+    smoke_value[int(request.form['smoke'])] = 1
+    features.extend(smoke_value)
     values = [np.array(features)]
     prediction = model.predict(values)
     if(prediction==1):
